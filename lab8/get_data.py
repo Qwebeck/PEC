c1f49f77c691b59d061a05d3925261e4dcd45b1f
Bohdan Forostianyi <bohdanforostianyi@gmail.com>
2020-12-06 21:44:08 +0100

    Adds lab 8

---
+++ lab8/get_data.py
@@ -0,0 +1,154 @@
+#	ssh -p2221 student@149.156.197.221
+#	sftp -P2221 student@149.156.197.221
+
+from __future__ import division
+from __future__ import print_function
+
+import time
+import os
+
+from usbtmcDevice import UsbtmcDevice
+
+d_path = 'figs/'
+
+# connect to instruments
+dev_scope = UsbtmcDevice('/dev/usbtmc0')
+id = dev_scope.ask('*idn?')
+print(id)
+
+dev_scope.write('chan1:disp on')
+dev_scope.write('chan1:probe 10')
+dev_scope.write('chan1:coupling DC')
+dev_scope.write('chan1:scale 0.5')
+dev_scope.write('chan1:offset -1.5')
+dev_scope.write('timebase:scale 1e-4')
+dev_scope.write(':TRIGger:SWEep Auto')
+dev_scope.write(':TRIGger:EDGe:LEVel 0.5')
+
+dev_scope.write('chan2:disp on')
+dev_scope.write('chan2:probe 1')
+
+dev_gen = UsbtmcDevice('/dev/usbtmc1')
+id = dev_gen.ask('*idn?')
+print(id)
+
+
+def strip_hdr(data):
+    assert data[0] == '#'
+    h_len = int(data[1])
+    s_num = int(data[2:2+h_len])
+    print('data_hdr: ' + data[0:2+h_len])
+    return bytearray(data[h_len+2:h_len+2+s_num])
+
+
+def get_display(fname):
+    dev_scope.write(':display:data? on,off,png')
+    time.sleep(1)
+    buf = dev_scope.read()
+    img_data = strip_hdr(buf)
+    fd = os.open(d_path + fname + '.png', os.O_CREAT | os.O_WRONLY)
+    os.write(fd, img_data)
+    os.close(fd)
+
+
+def get_wave(fname, ch='1'):
+    dev_scope.write('WAV:SOUR CHAN'+ch)
+    buf = dev_scope.ask('wav:data?')
+    data = strip_hdr(buf)
+
+    yref = float(dev_scope.ask('wav:yref?'))
+    yinc = float(dev_scope.ask('wav:yinc?'))
+    yori = float(dev_scope.ask('wav:yorigin?'))
+    tbase = float(dev_scope.ask('timebase:scale?'))
+    print('[yref, yinc, yori, tbase] =', [yref, yinc, yori, tbase])
+
+    samples = [((s-yref)-yori)*yinc for s in data]
+    fd = os.open(d_path + fname + '.param', os.O_CREAT | os.O_WRONLY)
+    os.write(fd, str([yref, yinc, yori, tbase]))
+    os.close(fd)
+    return data, samples, tbase
+
+
+def get_data2(duty, offset, coupling, scale):
+    fname = 'SQ_' + coupling + '_' + str(offset) + 'V_' + str(duty) + '%'
+    print(fname)
+    dev_scope.write('chan2:coupling ' + coupling)
+    dev_scope.write('chan2:offset ' + str(offset))
+    dev_scope.write('chan2:scale ' + str(scale))
+    time.sleep(1)
+    get_display(fname)
+
+    dev_scope.write('WAV:SOUR CHAN2')
+    time.sleep(1)
+    [data, samples, tbase] = get_wave(fname, '2')
+    fd = os.open(d_path + fname + '.raw', os.O_CREAT | os.O_WRONLY)
+    os.write(fd, data)
+    os.close(fd)
+
+
+def SQ_test():
+    freq = 2000
+    lo = 0
+    hi = 4
+    duty = 20
+    gen_squ(freq, lo, hi, duty)
+    get_data2(duty, offset=-2, coupling='DC', scale=1)
+    get_data2(duty, offset=0, coupling='AC', scale=1)
+
+    duty = 80
+    gen_squ(freq, lo, hi, duty)
+    get_data2(duty, offset=-2, coupling='DC', scale=1)
+    get_data2(duty, offset=0, coupling='AC', scale=1)
+
+
+def Z5():
+    freq = 1e5
+    lo = 0
+    hi = 4
+    duty = 50
+    gen_squ(freq, lo, hi, duty)
+    dev_scope.write(':TRIGger:EDGe:SOURce CHANnel2')
+
+    get_display('test')
+    dev_scope.write('timebase:scale 1e-8')
+    get_display('test')
+
+
+def gen_squ(freq, lo, hi, duty):
+    off = 0
+    dev_gen.write('output off')
+    dev_gen.write('func squ')
+    dev_gen.ask('func?')
+    dev_gen.write('freq %f' % freq)
+    dev_gen.write('func:squ:dcyc %f' % duty)
+
+    dev_gen.write('volt:offs %f' % off)
+    print('volt:offs? ' + dev_gen.ask('volt:offs?'))
+    time.sleep(1)
+
+    dev_gen.write('volt:low %f' % lo)
+    print('volt:low? ' + dev_gen.ask('volt:low?'))
+    time.sleep(1)
+
+    dev_gen.write('volt:high %f' % hi)
+    print('volt:high? ' + dev_gen.ask('volt:high?'))
+    time.sleep(1)
+
+    dev_gen.write('output on')
+    time.sleep(1)
+
+
+def gen_sin(freq, vpp):
+    freq = 1000
+    vpp = 10
+    offset = 0
+
+    dev_gen.write('output off')
+    dev_gen.write('func sin')
+    dev_gen.write('freq %f' % freq)
+    dev_gen.write('volt %f' % vpp)
+    dev_gen.write('volt:offs %f' % offset)
+    dev_gen.write('output on')
+
+
+get_display('test')
---
+++ lab8/postprocess.py
@@ -0,0 +1,167 @@
+from functools import partial
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
+import random
+from scipy import integrate
+import math
+
+"""
+Skrypt do ulatwienia pracy nad sprawozdaniem nr. 7
+Kod pozwala na processing danych, rysowanie wykresów oraz na wyliczenie odpowiednich statystyk.
+Użycie:
+$ python KOD_ZADANIA
+KOD_ZADANIA - krótka nazwa zadania. Teraz są obsługiwane:
+
+    1. zad_2 - zadanie 2
+    2. zad_5 - zadanie 5
+
+Przykład użycia:
+$ python  zad_2
+"""
+AVAILABLE_COLORS = ['blue', 'red', 'green', 'black']
+
+
+def analyse(*test_names, describe_data=None):
+    """
+    Draw plots for tests
+    """
+    plt.grid()
+    plt.xlabel('t')
+    plt.ylabel('V')
+    for test_name in test_names:
+        raw_data = read_raw_data(test_name)
+        data = trasnform_to_data(raw_data, test_name)
+        if describe_data:
+            describe_data(data, test_name)
+        display_data(data, test_name)
+    plt.legend()
+    plt.show()
+
+
+def read_raw_data(test_name):
+    """
+    Reads raw data from .raw files
+    """
+    with open(f"figs/{test_name}.raw", "rb") as f:
+        data = []
+        byte = f.read(1)
+        while byte:
+            data.append(byte)
+            byte = f.read(1)
+        data = np.array([int.from_bytes(b, 'little') for b in data])
+        return data
+
+
+def trasnform_to_data(raw_data, test_name):
+    """
+    Reads params yref, yinc, yori from tests .param file and applies
+    (data - yref - yori) * yinc transformer function to them. 
+    """
+    with open(f"figs/{test_name}.param", "r") as f:
+        params = f.readline()[1:-1]
+        yref, yinc, yori, tbase = [float(p) for p in params.split(", ")]
+        def transfromer(data): return (data - yref - yori) * yinc
+        trasnformed_data = [*map(transfromer, raw_data)]
+        time = np.arange(0, len(trasnformed_data)) * tbase
+        return np.vstack([time, trasnformed_data]).T
+
+
+def describe_voltage_flow(data, test_name):
+    """
+    Prints period and DC coefficient to stdout
+    """
+    t, voltage_flow = get_period_data(data)
+    a_0 = get_DC_coeff(t, voltage_flow)
+    print(f'Data for {test_name}: ')
+    print('a_0: ', a_0)
+    print('T: ', t)
+
+
+def get_period_data(data):
+    """
+    Crops data to data from one period. 
+    Period caclulated as distance between two highest picks.
+    """
+    time, voltage = data[:, 0], data[:, 1]
+    m_voltage = max(voltage)
+    occurences = np.where(voltage == m_voltage)
+    indexes = occurences[0]
+    start, end = indexes[0], indexes[1]
+    return abs(time[start] - time[end]), data[start:end, :]
+
+
+def get_DC_coeff(period, period_voltage_flow):
+    """
+    Calculates coefficient of DC component in data
+    """
+    time, voltage = period_voltage_flow[:, 0], period_voltage_flow[:, 1]
+    return 1/period * integrate.simps(voltage, time)
+
+
+def describe_rise_time(data, test_name):
+    """
+    Prints to stdout information about rise time 
+    """
+    t_r_osc_m = get_rise_time(data)  # zmierzony czas narastania oscyloskopu
+    f_o_max = 50
+    t_r_osc = to_s(350 / f_o_max)  # czas narastania przebiegu prosokatnego
+    t_r_gen = math.sqrt(t_r_osc_m ** 2 - t_r_osc**2)
+    print(f'{test_name}')
+    print('Parametr                  | Wartość |  Jednostka')
+    print('--------------------------------------')
+    print(f'Czas narastania t_n_osc   | {to_ns(t_r_osc)}  | ns')
+    print(f'Czas narastania zmierzony | {to_ns(t_r_osc_m)} | ns')
+    print(f'Czas narastania t_n_gen   | {to_ns(t_r_gen)} | ns')
+
+
+def get_rise_time(data):
+    """
+    Calculates rise time for data. 
+    """
+    time, voltage = data[:, 0], data[:, 1]
+    max_value = max(voltage)
+    lower_bound_ind = np.where(voltage >= 0.1 * max_value)[0][0]
+    upper_bound_ind = np.where(voltage >= 0.9 * max_value)[0][0]
+    return time[upper_bound_ind] - time[lower_bound_ind]
+
+
+def to_s(ns):
+    return ns * 10 ** -9
+
+
+def to_ns(s):
+    return s * 10 ** 9
+
+
+def display_data(data, test_name):
+    """
+    Display voltage plot, maximum and minimum levels.
+    """
+    global AVAILABLE_COLORS
+    color = random.choice(AVAILABLE_COLORS)
+    AVAILABLE_COLORS = [c for c in AVAILABLE_COLORS if c != color]
+    time, voltage = data[:, 0], data[:, 1]
+    v_max, v_min = max(voltage), min(voltage)
+    line = np.ones((len(voltage),))
+    plt.plot(time, line * v_max, color=color,
+             linestyle='dashed', label=f'{test_name} voltage bounds')
+    plt.plot(time, line * v_min, color=color,
+             linestyle='dashed')
+    plt.plot(time, voltage, color, label=test_name)
+
+
+def run(task_code):
+    """
+    Runs analysis for task with given task_code
+    """
+    default_callback = partial(
+        print, f'Task code {task_code} is not a valid task code')
+    {
+        'zad_2': partial(analyse, 'SQ_DC_0V_20%', 'SQ_AC_0V_20%', describe_data=describe_voltage_flow),
+        'zad_5': partial(analyse, 'Z5', describe_data=describe_rise_time)
+    }.setdefault(task_code, default_callback)()
+
+
+if __name__ == '__main__':
+    run(sys.argv[1])
