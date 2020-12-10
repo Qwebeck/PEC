1d9bba4d6a3c4490b37c208533fb939b5b66da2e
Bohdan Forostianyi <bohdanforostianyi@gmail.com>
2020-12-10 15:35:12 +0100

    ramp slow

--- lab9/ramp_slow.py
+++ lab9/ramp_slow.py
@@ -1,1 +1,20 @@
 from machine import Pin
+def start():
+    scl = Pin(5, Pin.OUT)
+    sda = Pin(4, Pin.OPEN_DRAIN)
+    return scl, sda
+
+def shift(sda):
+    addr = [1,0,0,1,1,1,1]
+    mode = 1
+    for bit in addr:
+        sda(bit)
+    sda(mode)
+
+def stop(scl):
+    return scl()
+
+
+scl, sda = start()
+shift(sda)
+print(scl())
